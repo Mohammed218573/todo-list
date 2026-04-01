7b64ead95c143a117d7602cf59bb95f44b6cf556
mohammed <ma0765746@gmail.com>
2026-04-01 19:14:50 +0300

    Add local storage feature

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
+let tasks = [];
+const savedTasks = localStorage.getItem("tasks");
+if(savedTasks){
+    tasks = JSON.parse(savedTasks);
+}
+tasks.forEach(function(task){
+    const li = document.createElement("li");
+    li.textContent = task;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑";
+    deleteBtn.classList.add("delete-btn")
+    deleteBtn.addEventListener("click",function(){
+        li.remove();
+        tasks = tasks.filter(function(t){
+            return t !== task; 
+        })
+        localStorage.setItem("tasks",JSON.stringify(tasks))
+    })
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+})
+    
+
 addBtn.addEventListener("click" ,function(){
     const taskText = taskInput.value.trim();
     if (taskText === ""){
@@ -8,6 +31,8 @@ addBtn.addEventListener("click" ,function(){
     }
     const li = document.createElement("li");
     li.textContent = taskText;
+    tasks.push(taskText);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
     li.addEventListener("click",function(){
         li.classList.toggle("completed");
     })
@@ -16,6 +41,11 @@ addBtn.addEventListener("click" ,function(){
     deleteBtn.classList.add("delete-btn")
     deleteBtn.addEventListener("click",function(){
         li.remove();
+        tasks = tasks.filter(function(task){
+            return task !== taskText; 
+        })
+        localStorage.setItem("tasks",JSON.stringify(tasks))
+        
     })
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
